66841784c424c83b210d215481f717cbed405701
Yannik Gerlach <9513476+yannikgerlach@users.noreply.github.com>
2025-01-08 06:53:39 +0100

    make sending optional in sh script

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@
 poetry run python scripts/download.py
 poetry run python scripts/process.py
 poetry run python scripts/plot.py
-poetry run python scripts/send.py
+if [ "$1" == "--send" ]; then
+    poetry run python scripts/send.py
+fi
