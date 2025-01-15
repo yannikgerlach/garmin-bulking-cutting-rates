da7fa40a01f79bf19b5a3b63302af094626aa673
Yannik Gerlach <9513476+yannikgerlach@users.noreply.github.com>
2025-01-15 07:34:53 +0100

    select storage directory

--- scripts/files.py
+++ scripts/files.py
@@ -1,3 +1,26 @@
-RAW_DATA_FILE = "weight_raw.json"
-DAILY_DATA_FILE = "weight_daily.csv"
-WEEKLY_DATA_FILE = "weight.csv"
+import os
+
+
+def get_storage_directory_env() -> str:
+    return os.getenv("STORAGE_DIRECTORY", "./")
+
+
+def get_storage_directory() -> str:
+    storage_directory = get_storage_directory_env()
+    if not os.path.exists(storage_directory):
+        raise RuntimeError(f"Storage directory {storage_directory} does not exist.")
+    if not os.access(storage_directory, os.R_OK):
+        raise RuntimeError(f"Storage directory {storage_directory} is not readable.")
+    return storage_directory
+
+
+def get_full_storage_path(filename: str) -> str:
+    return os.path.join(get_storage_directory(), filename)
+
+
+RAW_DATA_FILE = get_full_storage_path("weight_raw.json")
+DAILY_DATA_FILE = get_full_storage_path("weight_daily.csv")
+WEEKLY_DATA_FILE = get_full_storage_path("weight.csv")
+
+WEIGHT_CHANGE_PNG = get_full_storage_path("weight_change.png")
+WEIGHT_PNG = get_full_storage_path("weight.png")
--- scripts/send.py
+++ scripts/send.py
@@ -3,14 +3,16 @@ import os
 
 import requests
 
+from scripts.files import WEIGHT_CHANGE_PNG, WEIGHT_PNG
+
 
 def get_file_content_as_base64(path):
     with open(path, "rb") as file:
         return base64.b64encode(file.read()).decode("utf-8")
 
 
-weight_plot_base64 = get_file_content_as_base64("weight.png")
-weight_change_plot_base64 = get_file_content_as_base64("weight_change.png")
+weight_plot_base64 = get_file_content_as_base64(WEIGHT_PNG)
+weight_change_plot_base64 = get_file_content_as_base64(WEIGHT_CHANGE_PNG)
 
 response = requests.post(
     os.environ["WEBHOOK_URL"],
