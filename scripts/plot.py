fix calculation for beginning of the week

scripts/plot.py
@@ -52,21 +52,30 @@ def plot_figures():
     target_this_week_day = target_this_week[DATE_COLUMN].values[0]
     most_recent_reading_date = df_daily[DATE_COLUMN].max()
     remaining_days_this_week = (target_this_week_day - most_recent_reading_date).days
+    passed_days_this_week = 7 - remaining_days_this_week
 
-    raw_data_weight_first_days = df_daily["weight_in_grams"].tail(
-        7 - remaining_days_this_week
+    raw_data_weight_first_days = df_daily["weight_in_grams"].tail(passed_days_this_week)
+
+    number_of_days_to_look_back = max(
+        2, passed_days_this_week
+    )  # quadratic interpolation needs at least 2 points
+    last_7d_weights = (
+        df_daily["weight_in_grams"].tail(number_of_days_to_look_back).values.tolist()
+    )  # how many days to look back
+    number_of_last_weeks_days = max(
+        0, number_of_days_to_look_back - passed_days_this_week
     )
-
-    target_weight_sum = target_this_week_weight * 7 - sum(raw_data_weight_first_days)
-
-    full_target_weight_sum = sum(raw_data_weight_first_days) + target_weight_sum
+    raw_weight_last_days = last_7d_weights[:number_of_last_weeks_days]
+    target_weight_sum_with_last_days = target_this_week_weight * 7 + sum(
+        raw_weight_last_days
+    )
 
     # interpolate using pandas on the sums
     interpolate_df = pd.DataFrame()
     interpolate_df["weight"] = (
-        np.cumsum(raw_data_weight_first_days).values.tolist()
+        np.cumsum(last_7d_weights).tolist()
         + (remaining_days_this_week - 1) * [np.nan]
-        + [float(full_target_weight_sum)]
+        + [float(target_weight_sum_with_last_days)]
     )
     remaining_days_weight = (
         interpolate_df["weight"]
@@ -74,6 +83,23 @@ def plot_figures():
         .diff()
         .tail(remaining_days_this_week)
     )
+
+    # check correctness
+    targeted_weights_this_week = (
+        raw_data_weight_first_days.values.tolist()
+        + remaining_days_weight.values.tolist()
+    )
+    average_targeted_weight_this_week = (
+        np.mean(targeted_weights_this_week).round().astype(int)
+    )
+    if average_targeted_weight_this_week == target_this_week_weight:
+        print("Correctly calculated the target weight for this week")
+    else:
+        print(
+            "Error in calculating the target weight for this week: ",
+            average_targeted_weight_this_week,
+            target_this_week_weight,
+        )
 
     # Plot each series
     df_7d_last = df_7d.tail(1)