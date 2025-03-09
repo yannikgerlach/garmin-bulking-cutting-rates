49a55e02d5385b86251b06511fcaac687a200480
Yannik Gerlach <9513476+yannikgerlach@users.noreply.github.com>
2025-03-09 11:14:05 +0100

    revert

--- scripts/files.py
+++ scripts/files.py
@@ -26,12 +26,10 @@ def get_full_storage_path(filename: str) -> str:
     return os.path.join(get_storage_directory(), filename)
 
 
-storage_directory = get_storage_directory()
+RAW_DATA_FILE = get_full_storage_path("weight_raw.json")
+DAILY_DATA_FILE = get_full_storage_path("weight_daily.csv")
+WEEKLY_DATA_FILE = get_full_storage_path("weight.csv")
 
-RAW_DATA_FILE = os.path.join(storage_directory, "weight_raw.json")
-DAILY_DATA_FILE = os.path.join(storage_directory, "weight_daily.csv")
-WEEKLY_DATA_FILE = os.path.join(storage_directory, "weight.csv")
-
-WEIGHT_CHANGE_PNG = os.path.join(storage_directory, "weight_change.png")
-WEIGHT_PNG = os.path.join(storage_directory, "weight.png")
-REMAINING_DAYS_WEIGHT_PNG = os.path.join(storage_directory, "remaining_days_weight.png")
+WEIGHT_CHANGE_PNG = get_full_storage_path("weight_change.png")
+WEIGHT_PNG = get_full_storage_path("weight.png")
+REMAINING_DAYS_WEIGHT_PNG = get_full_storage_path("remaining_days_weight.png")
