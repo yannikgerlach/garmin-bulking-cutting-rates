3bf1741bc5bfa09fd34a619cc987ef6e4995b959
Yannik Gerlach <9513476+yannikgerlach@users.noreply.github.com>
2025-03-09 10:50:53 +0100

    fixes

--- scripts/download.py
+++ scripts/download.py
@@ -7,7 +7,7 @@ import garminconnect
 from scripts.files import RAW_DATA_FILE
 
 
-def load_and_store_gamin_data():
+def load_and_store_garmin_data():
     garmin = garminconnect.Garmin(
         os.getenv("GARMIN_EMAIL"), os.getenv("GARMIN_PASSWORD")
     )
@@ -20,11 +20,12 @@ def load_and_store_gamin_data():
     enddate = date.today().isoformat()
 
     data = garmin.get_weigh_ins(startdate, enddate)
-    assert data is not None
+    if data is None:
+        raise RuntimeError("Failed to retrieve data from Garmin")
 
     with open(RAW_DATA_FILE, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
 
 
 if __name__ == "__main__":
-    load_and_store_gamin_data()
+    load_and_store_garmin_data()
