e8fe93496f24f87da97c03c5fa2a25e03404591f
Yannik Gerlach <9513476+yannikgerlach@users.noreply.github.com>
2025-03-09 10:51:59 +0100

    Customize start date (#13)
    * add env variable for start date
    
    * fixes

--- scripts/download.py
+++ scripts/download.py
@@ -7,22 +7,25 @@ import garminconnect
 from scripts.files import RAW_DATA_FILE
 
 
-def load_and_store_gamin_data():
+def load_and_store_garmin_data():
     garmin = garminconnect.Garmin(
         os.getenv("GARMIN_EMAIL"), os.getenv("GARMIN_PASSWORD")
     )
     garmin.login()
     garmin.garth.dump(os.getenv("GARTH_HOME", "~/.garth"))
 
-    startdate = date(2024, 11, 1).isoformat()  # to do: allow user to specify start date
+    startdate = os.getenv("GARMIN_START_DATE", None)
+    if startdate is None:
+        raise RuntimeError("Environment variable GARMIN_START_DATE is not set")
     enddate = date.today().isoformat()
 
     data = garmin.get_weigh_ins(startdate, enddate)
-    assert data is not None
+    if data is None:
+        raise RuntimeError("Failed to retrieve data from Garmin")
 
     with open(RAW_DATA_FILE, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
 
 
 if __name__ == "__main__":
-    load_and_store_gamin_data()
+    load_and_store_garmin_data()
